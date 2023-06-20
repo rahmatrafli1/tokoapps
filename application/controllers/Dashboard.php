92c55ea64309677937d5b4648a1dacf306fe2649
rahmatrafli1 <rahmatrafli1@gmail.com>
2023-06-20 13:26:07 +0700

    mengubah autentifikasi menjadi lebih aman lagi

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,7 +6,7 @@ class Dashboard extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		if ($this->session->userdata('role_id') != 2) {
+		if (!$this->session->userdata('username')) {
 			$this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
                 <strong>Anda harus masuk terlebih dahulu!</strong>
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -14,6 +14,9 @@ class Dashboard extends CI_Controller
                 </button>
               </div>');
 			redirect('auth/login');
+		}
+		if ($this->session->userdata('role_id') == 1) {
+			redirect('admin/Dashboard_Admin');
 		}
 	}
 
--- application/controllers/admin/Profile.php
+++ application/controllers/admin/Profile.php
@@ -8,7 +8,7 @@ class Profile extends CI_Controller
         parent::__construct();
         if ($this->session->userdata('role_id') != 1) {
             $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <strong>Selain admin, dilarang masuk. Anda harus masuk terlebih dahulu sebagai admin!</strong>
+                <strong>Anda harus masuk terlebih dahulu sebagai admin!</strong>
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
